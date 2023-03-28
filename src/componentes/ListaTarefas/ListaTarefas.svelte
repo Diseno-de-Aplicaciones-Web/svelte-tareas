82b9f0342bdefb7ae69ab0bed3dd046db849e27e
Daniel Bañobre Dopico <bytes4life@proton.me>
2023-03-28 11:41:08 +0200

    ...

--- src/componentes/ListaTarefas/ListaTarefas.svelte
+++ src/componentes/ListaTarefas/ListaTarefas.svelte
@@ -2,7 +2,6 @@
 
     import Tarefa from "../Tarefa/Tarefa.svelte";
     import BorrarTarefa from "../BorrarTarefa/BorrarTarefa.svelte";
-    import EditarTarefa from "../EditarTarefa/EditarTarefa.svelte";
 
     export let propiedadeListaTarefas // Propiedade <ListaTarefas propiedadeListaTarefas=""/>
 
@@ -13,7 +12,6 @@
     <li>
         <Tarefa propiedadeTarefa={cadaTarefa}/>
         <BorrarTarefa tarefa={cadaTarefa}/>
-        <EditarTarefa bind:tarefa={cadaTarefa}/>
     </li>
     {/each}
 </ol>
--- src/componentes/Tarefa/Tarefa.svelte
+++ src/componentes/Tarefa/Tarefa.svelte
@@ -2,9 +2,54 @@
 
     export let propiedadeTarefa // Propiedade <Tarefa propiedadeTarefa=""/>
 
+    let estado = "ver"
+
+    let inputHTML
+
+    function manexadorEditar() {
+        estado = "editar"
+    }
+
+    function manexadorGardar() {
+        const tarefaJSON = JSON.stringify(propiedadeTarefa)
+        fetch(
+            "http://localhost:8000/tarefas/",
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: tarefaJSON
+            }
+        )
+        inputHTML.blur()
+        estado = "ver"
+    }
+
 </script>
 
 <label>
-    <span>{propiedadeTarefa.descripcion}</span>
-    <input type="checkbox" checked={propiedadeTarefa.completada}/>
+    <input bind:this={inputHTML} type="text" class={estado}
+        bind:value={propiedadeTarefa.descripcion}
+        on:focus={manexadorEditar}
+        on:blur={manexadorGardar}
+    />
+    {#if estado === "editar"}
+    <button>Guardar</button>
+    {/if}
+    <input type="checkbox"
+        bind:checked={propiedadeTarefa.completada}
+        on:input={manexadorGardar}
+    />
+
 </label>
+
+
+<style>
+    .ver {
+        border-style: none;
+    }
+    .editar {
+        background-color: greenyellow;
+    }
+</style>
